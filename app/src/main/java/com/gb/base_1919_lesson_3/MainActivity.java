a84ce31a10dddd8fcdf50c4c05548b58af5f4d26
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-27 21:52:43 +0300

    урок 3 третий способ "повесить" setOnClickListener

--- app/src/main/java/com/gb/base_1919_lesson_3/MainActivity.java
+++ app/src/main/java/com/gb/base_1919_lesson_3/MainActivity.java
@@ -8,13 +8,17 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    private int counterOne =0;
-    private int counterTwo =0;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private int counterOne = 0;
+    private int counterTwo = 0;
+    private int counterThree = 0;
     Button btnCounterOne;
     Button btnCounterTwo;
+    Button btnCounterThree;
     TextView tvCounterOne;
     TextView tvCounterTwo;
+    TextView tvCounterThree;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,34 +28,59 @@ public class MainActivity extends AppCompatActivity {
         btnCounterTwo.setOnClickListener(listener);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.btnCounterOne): {
+                counterOne = counterOne + 1;
+                tvCounterOne.setText(String.format("%d", counterOne));
+                break;
+            }
+            case (R.id.btnCounterTwo): {
+                counterTwo = counterTwo + 1;
+                tvCounterTwo.setText(String.format("%d", counterTwo));
+                break;
+            }
+            case (R.id.btnCounterThree): {
+                counterThree = counterThree + 1;
+                tvCounterThree.setText(String.format("%d", counterThree));
+                break;
+            }
+            default: {
+
+            }
+        }
+    }
+
     private void setListeners() {
         btnCounterOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                counterOne = counterOne+1;
+                counterOne = counterOne + 1;
                 tvCounterOne.setText(String.format("%d", counterOne));
             }
         });
         btnCounterTwo.setOnClickListener(listener);
+        btnCounterThree.setOnClickListener(this);
     }
 
 
-    View.OnClickListener listener= new View.OnClickListener() {
+    View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            switch (view.getId()){
-                case (R.id.btnCounterOne):{
-                    counterOne = counterOne+1;
+            switch (view.getId()) {
+                case (R.id.btnCounterOne): {
+                    counterOne = counterOne + 1;
                     tvCounterOne.setText(String.format("%d", counterOne));
                     break;
                 }
-                case (R.id.btnCounterTwo):{
-                    counterTwo = counterTwo+1;
+                case (R.id.btnCounterTwo): {
+                    counterTwo = counterTwo + 1;
                     tvCounterTwo.setText(String.format("%d", counterTwo));
                     break;
                 }
-                default:{
+                default: {
 
                 }
             }
@@ -60,11 +89,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initView() {
-        btnCounterOne= findViewById(R.id.btnCounterOne);
-        tvCounterOne= findViewById(R.id.tvCounterOne);
+        btnCounterOne = findViewById(R.id.btnCounterOne);
+        tvCounterOne = findViewById(R.id.tvCounterOne);
 
-        btnCounterTwo= findViewById(R.id.btnCounterTwo);
-        tvCounterTwo= findViewById(R.id.tvCounterTwo);
+        btnCounterTwo = findViewById(R.id.btnCounterTwo);
+        tvCounterTwo = findViewById(R.id.tvCounterTwo);
+
+        btnCounterThree = findViewById(R.id.btnCounterThree);
+        tvCounterThree = findViewById(R.id.tvCounterThree);
     }
 
     @Override
@@ -76,4 +108,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
     }
+
+
 }
