ec362773c95be0d5996b730d76bb09de73cf9e82
Andej Nesterenko <mentatus.n@gmail.com>
2022-01-27 20:13:58 +0300

    урок 3, вернули java

---
+++ app/src/main/java/com/gb/base_1919_lesson_3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gb.base_1919_lesson_3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
